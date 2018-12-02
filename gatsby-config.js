8a62a78399443690f5b4ef58070b3710d90a79ce
Jason Geiger <jjgeiger@130public.net>
2018-12-02 10:49:28 -0800

    gatsby.config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ try {
 
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+    spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || ''
 }
 
 const { spaceId, accessToken } = contentfulConfig
