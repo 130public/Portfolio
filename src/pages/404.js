9bc9aae9e8a11186981bee38de470b292f69762d
Jason Geiger <448523+jasonjgeiger@users.noreply.github.com>
2019-05-06 13:01:17 -0700

    404 style tweak

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import Page from '../components/page'
-
+import Hero from '../components/hero'
 
 
 class Fun extends React.Component {
   render() {
 
-  
-
     return (
-      <Page>
-          404
-        </Page>
+      <Page className="fun">
+        <Hero title="404" body="Not found" style="blank" className="fullHeight" />
+      </Page>
     )
   }
 }
