2a6f9e6afa224fb818e7f2e5b20175b52a61102e
Jason Geiger <448523+jasonjgeiger@users.noreply.github.com>
2019-07-18 12:04:32 -0700

    Revert "404 style tweak"
    This reverts commit 9bc9aae9e8a11186981bee38de470b292f69762d.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Page from '../components/page'
-import Hero from '../components/hero'
+
 
 
 class Fun extends React.Component {
   render() {
 
+  
+
     return (
-      <Page className="fun">
-        <Hero title="404" body="Not found" style="blank" className="fullHeight" />
-      </Page>
+      <Page>
+          404
+        </Page>
     )
   }
 }
